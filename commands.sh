eb646c471038b93d2f854a2758b4a3cb983d3300
Stefan Tertan <stefantertan@yahoo.com>
2018-11-03 01:13:38 +0000

    fix in `commands.sh` -> forgot to rename directory!

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 protoc -I=simple/ --python_out=simple/ simple/simple.proto
-protoc -I=enum/ --python_out=enum/ enum/enum_example.proto
+protoc -I=enums/ --python_out=enums/ enums/enum_example.proto
 protoc -I=complex/ --python_out=complex/ complex/complex.proto
